ba1ef100224b7f8a3485717f056a1f182c693308
Sen Lin <33269903+rlinh99@users.noreply.github.com>
2021-11-05 00:26:40 -0700

    finished select and mutate
    finished mutate and crossover

--- actions.py
+++ actions.py
@@ -3,37 +3,57 @@ from evaluations import *
 from helpers import *
 import random
 
-
 population_pool = []
-mating_poll = []
+mating_pool = []
 
+
+# mutate by in place exchange
 def mutate(chromosome):
     x, y = get_mutate_points()
-    print("test")
-    print(x)
-    print(y)
-    return
+    temp = chromosome[x - 1]
+    chromosome[x - 1] = chromosome[y - 1]
+    chromosome[y - 1] = temp
+    return chromosome
 
 
 def crossover():
     return
 
-# create default population pool according to the sizedefined in defaultData.py
+
+# using weight average selection by rank
+def select():
+    selected = []
+    sum_of_rank = (DEFAULT_POP_SIZE*(DEFAULT_POP_SIZE + 1)) / 2
+    for i in range(0, DEFAULT_SELECTION_SIZE):
+        target = random.uniform(0, 1)
+        accumulated = 0
+        for index, pop in enumerate(population_pool):
+            rank = 30 - index
+            prob = rank/sum_of_rank
+            accumulated = accumulated + prob
+            if accumulated > target:
+                selected.append(pop)
+                print(pop)
+                break
+    print(selected)
+    print(len(selected))
+    return
+
+
+# create default population pool according to the size defined in defaultData.py
 # insert default population
 def create_default_populations():
-    for x in range(PROJECT_DEFAULT_POPULATION_SIZE):
+    while len(population_pool) != DEFAULT_POP_SIZE:
         sample = random.sample(range(1, 11), 10)
         if sample not in population_pool:
             population_pool.append((sample, fitness(sample)))
-
-
-def evaluate_initial_cost():
-    return
+    population_pool.sort(key=lambda tup: tup[1])
 
 
 def run():
     create_default_populations()
-    mutate("bbb")
-    a = fitness([1,2,3,4,5,6,7,8,9,10])
-    print("test")
-    print(a)
+    select()
+    example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # result = mutate(example)
+    # print(result)
+    # a = fitness(example)
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from actions import *
 
 
 if __name__ == '__main__':
-    print(PROJECT_DEFAULT_DATA[1])
     run()
 
 
